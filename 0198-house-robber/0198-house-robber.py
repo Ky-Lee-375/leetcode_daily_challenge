ba9192951d4358092d4171f8b7963b5986700868
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2023-03-27 17:22:16 -0500

    Time: 36 ms (41.97%), Space: 13.8 MB (53.76%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -12,17 +12,3 @@ class Solution:
         for i in range(2,len(nums)):
             sumlist[i] += max(sumlist[i-1], sumlist[i-2]+nums[i])
         return sumlist[-1]
-
-    
-#         if len(nums) <= 2:
-#             return max(nums)
-        
-#         dp = [0] * (len(nums) + 1)
-        
-#         dp[1] = nums[0]
-            
-#         for i in range(1, len(nums)):
-#             dp[i+1] = max(dp[i], dp[i-1] + nums[i])
-        
-#         return dp[-1]
-        
