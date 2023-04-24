5e659fe1325583e5ab1c12e5e7dc684959c0e69c
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2023-04-24 13:42:21 -0500

    Time: 382 ms (88.93%), Space: 14.2 MB (99.07%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -0,0 +1,17 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        ValueCounter = list(collections.Counter(tasks).values()) 
+        
+        maxCount = max(ValueCounter) 
+        num_of_chars_with_max_count = ValueCounter.count(maxCount) 
+        
+        num_of_chunks_with_idles = maxCount-1 
+
+        length_of_a_chunk_with_idle = n+1  
+
+        length_of_the_final_chunk = num_of_chars_with_max_count  # 2  
+
+        length_of_all_chunks = (num_of_chunks_with_idles*length_of_a_chunk_with_idle) + length_of_the_final_chunk 
+
+        return max(len(tasks), length_of_all_chunks)
+        
