f3e407f7faaf9fa49eb261cf46814a5d2ae53f1a
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-04-20 18:17:16 -0500

    Time: 697 ms (49.05%), Space: 53.2 MB (30.34%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -7,12 +7,14 @@ class Solution:
             # run out of interval
             if i == len(intervals):
                 return 0
+            # if already computed,return the cache
             if i in cache:
                 return cache[i]
             
             # dont include
             res = dfs(i+1)
             # include
+            # binary seach the next job
             j = bisect.bisect(intervals, (intervals[i][1], -1, -1))
             cache[i] = res = max(res, intervals[i][2] + dfs(j))
             return res
