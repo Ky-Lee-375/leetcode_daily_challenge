e56a6ebf0e51604d048c89f3d3769ba78ae46102
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-04-21 00:18:30 -0500

    Time: 38 ms (49.94%), Space: 16.6 MB (80.08%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -1,13 +1,17 @@
 class Solution:
     def countOrders(self, n: int) -> int:
-        # 2n slots: pickup and dropoff
-        # x*(x-1)/2 -> valid choices
+        # 2n slots per each order: pickup and dropoff
         
         slots = 2*n
+        # n *(n-1) /2 -> valid combination
         output = 1
         while slots > 0:
-            valid = slots*(slots-1)//2
+            valid = slots * (slots-1) // 2
             output *= valid
             slots -= 2
-        return output % (10**9 +7)
+        
+        return output % (10**9+7)
+        
+        
+     
         
