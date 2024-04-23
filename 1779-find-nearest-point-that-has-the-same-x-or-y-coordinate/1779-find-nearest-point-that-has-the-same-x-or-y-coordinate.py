b8d5ee86d99a13f82c072f25848d3e98807f46df
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-04-23 13:49:35 -0500

    Time: 609 ms (16.72%), Space: 21.8 MB (78.33%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
@@ -1,20 +1,27 @@
 class Solution:
     def nearestValidPoint(self, x: int, y: int, points: List[List[int]]) -> int:
-        if len(points) == 0:
-            return -1
-        if len(points) == 1 and (x == points[-1][0] and y == points[-1][1]):
-            return 0
-
-        rest = float('inf')
-        idx = -1
-        for i in range(len(points)):
+        n = len(points)
+        hashmap = {}
+        
+        for i in range(n):
             if points[i][0] == x or points[i][1] == y:
-                dist = abs(x - points[i][0] )  + abs(y - points[i][1])
-                if dist < rest:
-                    rest = dist
-                    idx = i
-        return idx
+                dist = abs(x-points[i][0]) + abs(y - points[i][1])
+                if dist in hashmap:
+                    hashmap[dist] = min(hashmap[dist], i)
+                else:
+                    hashmap[dist] = i
+        print(hashmap)
+        res = -1
+        if hashmap:
+            res = hashmap[min(hashmap.keys())]
+        return res
                 
-        
-            
-        
+    
+    # clarifying
+        # return the index of valid point w smallest dist
+            # otherwise return -1
+        # Can I assume points list always exsit?
+    
+    # algorithm
+    # hashmap (dist, [index])
+    # return min idx of min dist
