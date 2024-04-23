c93ed74fde6d6791a046489db37b9e2577031c45
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-04-23 00:06:55 -0500

    Time: 308 ms (64.96%), Space: 19 MB (56.80%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,35 +1,40 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        # edge case
         if not matrix:
             return 0
+        
         ROWS, COLS = len(matrix), len(matrix[0])
         
-        # direction: left, right, up, down
-        directions = [(0,1), (1,0), (0,-1), (-1,0)]
-        res = 0
-        # DFS -> maintain a visited set
+        # visited list
         visited = [[-1]*COLS for _ in range(ROWS)]
-        def dfs(x, y):
-            if visited[x][y] != -1:
-                return visited[x][y]
-                
-            res = 1
-            for d in directions:
-                next_x = x + d[0]
-                next_y = y + d[1]
-                counter = 1
-                if 0 <= next_x < ROWS and 0 <= next_y < COLS:
-                    if matrix[x][y] < matrix[next_x][next_y]:
-                        counter = 1 + dfs(next_x, next_y)
-                res = max(res, counter)
-            visited[x][y] = res
-            return res
+        # direction
+        direction = [(0,1), (0,-1), (1,0), (-1,0)]
+        # dfs
+        def dfs(r, c):
+            if visited[r][c] != -1:
+                return visited[r][c]
+            counter = 1
             
-        for rows in range(ROWS):
-            for cols in range(COLS):
-                res = max(dfs(rows, cols), res)
-        return res
+            for d in direction:
+                res = 1
+                next_r, next_c = r + d[0], c + d[1]
+                if 0<= next_r < ROWS and 0 <= next_c < COLS:
+                    if matrix[r][c] < matrix[next_r][next_c]:
+                        res = 1 + dfs(next_r, next_c)
+                counter = max(counter, res)
+            visited[r][c] = counter
+            return counter
+        
+        max_path = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                max_path = max(max_path, dfs(r,c))
+        return max_path
+                
+    
+# 1. Print the longest path
+# 2. Print all the paths that are strictly increasing.
+# 3. Print all non-decreasing path
                     
         
         
