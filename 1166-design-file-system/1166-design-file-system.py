932df428ae2b03c085a3facc8bd944418ee2187e
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-04-23 13:06:43 -0500

    Time: 215 ms (77.09%), Space: 24 MB (98.67%) - LeetHub

--- 1166-design-file-system/1166-design-file-system.py
+++ 1166-design-file-system/1166-design-file-system.py
@@ -5,25 +5,27 @@ class FileSystem:
         
 
     def createPath(self, path: str, value: int) -> bool:
-        if path in self.paths or len(path) ==0 or path == "/":
+        # check if path already exists
+        if not path or not value or path in self.paths:
             return False
+        # if so, return false
         
-        # rfind return the highest index val of the last occurrence of the substring from the input string
-        idx = path.rfind('/')
-        
-        # if it is initial path, "/" can be valid as well
-        if path[:idx] not in self.paths and len(path[:idx]) > 1:
+        dash_idx = path.rfind('/')
+        # find the last occurence of /
+        # if everything before doesn't exist-> return false
+        if path[:dash_idx] not in self.paths and len(path[:dash_idx]) > 1:
             return False
+        # if exists, add the new item
         self.paths[path] = value
         return True
         
 
     def get(self, path: str) -> int:
-        return self.paths.get(path, -1)
-        # if path in self.paths:
-        #     return self.paths[path]
-        # else:
-        #     return -1
+        if path in self.paths:
+            return self.paths[path]
+        else:
+            return -1
+        
         
         
 
@@ -32,3 +34,7 @@ class FileSystem:
 # obj = FileSystem()
 # param_1 = obj.createPath(path,value)
 # param_2 = obj.get(path)
+
+# Clarifying questions
+# "/" can be valid if it is the first path
+# single string and val to store
