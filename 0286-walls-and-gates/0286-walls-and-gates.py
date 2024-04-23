c70108e2e7cae37e713bf91c06418db914ab4d3d
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-04-23 13:32:54 -0500

    Time: 200 ms (42.96%), Space: 23 MB (34.15%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -3,33 +3,50 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        # 2147483647 is empty
-        #BFS
+        # initialize ROWS and COLS
         ROWS, COLS = len(rooms), len(rooms[0])
-        visit = set()
+        # queue to keep track of loc
         q = deque()
+        # visit set
+        visit = set()
+        # directions or helper function
+        directions = [(0,1), (0,-1), (1,0), (-1,0)]
         
-        def addRooms(r,c):
-            if (r < 0) or (r >= ROWS) or (c < 0) or (c >= COLS) or rooms[r][c] == -1 or (r,c) in visit:
-                return
-            q.append([r,c])
-            visit.add((r,c))
-        
+        # add all gates to queue
         for r in range(ROWS):
             for c in range(COLS):
                 if rooms[r][c] == 0:
-                    q.append([r,c])
+                    q.append((r,c))
                     visit.add((r,c))
+        
         dist = 0
+        # if queue exists:
         while q:
+        # pop the value
             for i in range(len(q)):
                 r,c = q.popleft()
-                rooms[r][c] =dist
-                addRooms(r+1, c)
-                addRooms(r-1, c)
-                addRooms(r, c+1)
-                addRooms(r, c-1)
-            
+                # assign dist to the loc
+                rooms[r][c] = dist
+                # move to available direction
+                for d in directions:
+                    next_r, next_c = r + d[0], c + d[1]
+                    if  0 <= next_r < ROWS and 0 <= next_c < COLS and (next_r, next_c) not in visit and rooms[next_r][next_c] != -1:
+                        q.append((next_r, next_c))
+                        visit.add((next_r, next_c))
                 
             dist += 1
+                
         
+       
+        
+        # clarifying questions:
+            # is it possible to not have any gates? == only walls?
+            # direction -> no diagonal?
+            # no return value?
+        
+        # algorithms
+            # bfs
+            # using dist variable start from 0 at gate
+            # invoke simultaneous call from every gate
+            # increment the distance after finding all distance
+            
