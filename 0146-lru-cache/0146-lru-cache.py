8dadd098caf26abea43f7adeae455ec15772866a
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-04-23 21:02:50 -0500

    Time: 579 ms (50.09%), Space: 77.8 MB (45.89%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,25 +1,49 @@
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev, self.next = None, None
+        
 class LRUCache:
-
+    
     def __init__(self, capacity: int):
-        # initialize w postive size capacity 
-        self.cache = OrderedDict()
+        self.cache = {}
         self.size = capacity
-
+        
+        self.left, self.right = Node(0,0), Node(0,0)
+        self.left.next, self.right.prev =self.right, self.left
+    
+    def remove(self, node):
+        pr, nxt = node.prev, node.next
+        pr.next = nxt
+        nxt.prev = pr
+        
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = node
+        nxt.prev = node
+        node.prev = prev
+        node.next = nxt
+    
     def get(self, key: int) -> int:
         if key in self.cache:
-            self.cache.move_to_end(key)
-            return self.cache[key]
-        else:
-            return -1
-        
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.cache.move_to_end(key)
+            self.remove(self.cache[key])
+        put_node = Node(key, value)
+        self.cache[key] = put_node
+        self.insert(put_node)
         
-        self.cache[key] = value
         if len(self.cache) > self.size:
-            self.cache.popitem(False)
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
 
         
         
@@ -29,3 +53,12 @@ class LRUCache:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+
+# clarifying question
+# if value is put to already existing key
+# doens't output anything?
+
+# algorithm
+# doubly linked list
+# class Node
+# lru on the left, most recent on the right
