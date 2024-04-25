511507730bbfef64167c41daba5406b93c6d6129
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-04-25 16:05:19 -0500

    Time: 184 ms (99.95%), Space: 24.4 MB (45.36%) - LeetHub

--- 1166-design-file-system/1166-design-file-system.py
+++ 1166-design-file-system/1166-design-file-system.py
@@ -5,29 +5,18 @@ class FileSystem:
         
 
     def createPath(self, path: str, value: int) -> bool:
-        # check if it already exists
         if path in self.paths:
-        # if so, return false
             return False
         
         idx = path.rfind("/")
-        # find the up until last /
-        parent = path[:idx]
-        if parent not in self.paths and len(parent) > 1:
-        # if that X exist in path and it's greater than 1
+        if path[:idx] not in self.paths and len(path[:idx]) > 1:
             return False
-        # return false
-        
-        # otherwise, add a new path
         self.paths[path] = value
         return True
-
+        
 
     def get(self, path: str) -> int:
-        return self.paths[path] if path in self.paths else -1
-        
-
-        
+        return self.paths.get(path) if path in self.paths else -1
         
 
 
@@ -35,7 +24,3 @@ class FileSystem:
 # obj = FileSystem()
 # param_1 = obj.createPath(path,value)
 # param_2 = obj.get(path)
-
-# Clarifying questions
-# "/" can be valid if it is the first path
-# single string and val to store
