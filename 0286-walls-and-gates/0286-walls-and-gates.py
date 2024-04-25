8305c6e23096742c50fae22205d0667ddfe25e67
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-04-25 16:19:31 -0500

    Time: 188 ms (58.37%), Space: 23 MB (34.09%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -3,50 +3,36 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        # initialize ROWS and COLS
+        # initialize variables
+        # row, col, direction, queue
         ROWS, COLS = len(rooms), len(rooms[0])
-        # queue to keep track of loc
-        q = deque()
-        # visit set
-        visit = set()
-        # directions or helper function
         directions = [(0,1), (0,-1), (1,0), (-1,0)]
+        q = deque()
+        visit = set()
         
-        # add all gates to queue
+        # add gates to queue
         for r in range(ROWS):
             for c in range(COLS):
                 if rooms[r][c] == 0:
                     q.append((r,c))
                     visit.add((r,c))
-        
+
+        # while queue exists
+        # bfs with queue
+        # pop the item and call bfs recursive
+        # increment dist after poping everything in the same level
         dist = 0
-        # if queue exists:
         while q:
-        # pop the value
             for i in range(len(q)):
-                r,c = q.popleft()
-                # assign dist to the loc
+                r, c = q.popleft()
                 rooms[r][c] = dist
-                # move to available direction
                 for d in directions:
-                    next_r, next_c = r + d[0], c + d[1]
-                    if  0 <= next_r < ROWS and 0 <= next_c < COLS and (next_r, next_c) not in visit and rooms[next_r][next_c] != -1:
+                    next_r, next_c = r+d[0], c + d[1]
+                    if 0<=next_r< ROWS and 0<=next_c< COLS and rooms[next_r][next_c] != -1 and (next_r, next_c) not in visit:
                         q.append((next_r, next_c))
                         visit.add((next_r, next_c))
-                
             dist += 1
-                
+                            
+                        
         
-       
         
-        # clarifying questions:
-            # is it possible to not have any gates? == only walls?
-            # direction -> no diagonal?
-            # no return value?
-        
-        # algorithms
-            # bfs
-            # using dist variable start from 0 at gate
-            # invoke simultaneous call from every gate
-            # increment the distance after finding all distance
-            
