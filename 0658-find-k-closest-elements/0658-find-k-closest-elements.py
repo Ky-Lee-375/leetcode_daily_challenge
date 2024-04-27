f43113565650eb22b7256aad28671150adce0774
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-04-26 21:05:43 -0500

    Time: 199 ms (99.00%), Space: 18.1 MB (81.59%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,28 +1,12 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        # edge case:
-        if len(arr) == k:
-            return arr
-        
-        # binary search
-        # find the index of the closest occurrence
-        idx = bisect.bisect_left(arr, x)
-        # *** left could be -1
-        left, right = idx -1, idx
-        
-        # two pointers: left and right
-        # while right - left - 1 < k
-        # if |left-x| < |right-x|
-            # increment right
-        while right - left -1 < k:
-            if left == -1:
-                right += 1
-                continue
-            if right == len(arr) or (left >=0 and abs(arr[left]-x) <= abs(arr[right]-x)):
-                left -=1
+        low, high = 0, len(arr) - k
+        while low < high:
+            mid = (low + high) // 2
+            # Shift the window to right if the element at mid+k is closer or the same distance but higher than arr[mid]
+            if x - arr[mid] > arr[mid + k] - x:
+                low = mid + 1
             else:
-                right += 1
-            
-        # slice from left to right
-        # return that array
-        return arr[left+1:right]
+                high = mid
+        # can manually build it as well: O(log (n-k))
+        return arr[low:low + k]
