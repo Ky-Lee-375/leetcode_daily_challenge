f4f6f88f1551f684f84a6cc4f78ac37ca139b600
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-05-07 14:44:38 -0500

    Time: 77 ms (7.36%), Space: 21 MB (19.58%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -6,26 +6,23 @@
 #         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        # base case
-        # when root is none -> return -inf
         if root == None:
-            return float('-inf')
+            return float("-inf")
         
-        # consider the negative values
-        res = float('-inf')
-        # recursively find the max
-        def dfs_max(node):
+        res =  float("-inf")
+        # dfs
+        def dfs(node):
             nonlocal res
-            # base case
             if node == None:
                 return 0
-            # get the right tree
-            # compare with 0?
-            right = max(0, dfs_max(node.right))
-            left = max(0, dfs_max(node.left))
+        # compare right and left
+            right = max(0, dfs(node.right))
+            left = max(0, dfs(node.left))
+            res = max(res, node.val+right+left)
             
-            curr = node.val + right + left
-            res = max(res, curr)
-            return node.val + max(right, left)
-        dfs_max(root)
+            # keep the larger one
+            return node.val + max(left, right)
+        # return the val of current node
+        dfs(root)
         return res
+        
