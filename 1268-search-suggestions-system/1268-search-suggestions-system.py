464c388f1d2f1d47e2241670ca522d5af1b585bf
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-05-08 10:06:14 -0500

    Time: 66 ms (91.98%), Space: 19.6 MB (81.39%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,31 +1,24 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        # declare res array
-        res = []
-        
-        # maintain two pointers
         # left, right
+        res =[]
         left, right = 0, len(products)-1
         
-        # sort the array
-        products = sorted(products)
-        
-        # go through letter in searchWord
-        # if it matches
-        # add first three or min (right - left +1)
-        # apped to the res
+        products.sort()
+        # for each char in searchword
+        # while
         for i in range(len(searchWord)):
             c = searchWord[i]
-            while left <= right and (len(products[left]) <= i or c != products[left][i]):
+            while left <= right and (len(products[left]) <= i or products[left][i] != c):
                 left += 1
-            while left <= right and (len(products[right]) <= i or c != products[right][i]):
+            while left <= right and (len(products[right]) <= i or products[right][i] != c):
                 right -= 1
             
-            res.append([])
-            remain = min(3, right-left+1)
-            for j in range(remain):
-                res[-1].append(products[left+j])
+            mid_res = []
+            num = min(3, right - left + 1)
+            for j in range(num):
+                mid_res.append(products[left+j])
             
+            res.append(mid_res)
         return res
-        # return res
         
