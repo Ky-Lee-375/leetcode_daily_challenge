299cdf3af3ffc2f29d7d3559ff7bafc643fbcd22
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-05-08 00:29:14 -0500

    Time: 265 ms (84.80%), Space: 19.7 MB (60.05%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
@@ -1,19 +1,17 @@
 class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
-        # edge case
         if min(difficulty) > max(worker):
             return 0
         
-        # zip difficulty and profit
-        # sort by difficulty
         jobs = sorted(zip(difficulty, profit))
-        print(jobs)
-        curr = j = res = 0
         worker = sorted(worker)
+        
+        cur = j = res = 0
         for i in range(len(worker)):
-            while j < len(jobs) and jobs[j][0] <= worker[i]:
-                curr = max(curr, jobs[j][1])
+            while j < len(jobs) and worker[i] >= jobs[j][0]:
+                cur = max(cur, jobs[j][1])
                 j += 1
-            res += curr        
+            res += cur
         return res
             
+        
