f9aa40ce01c003dcf47a8c41deec986d1d1ecfd8
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-05-08 15:30:11 -0500

    Time: 1967 ms (89.43%), Space: 33.6 MB (63.08%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -1,33 +1,33 @@
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
         ROWS, COLS = len(grid1), len(grid1[0])
+        counter = 0
         directions = [(0,1), (0,-1), (1,0), (-1,0)]
-                
-        # dfs (r, c, grid)
-        # recursively mark that island as 0
-        def dfs(r, c, grid):
+        
+        # dfs(r,c,grid)
+        # from r,c change all 1 to 0
+        def dfs(r,c, grid):
             grid[r][c] = 0
             
             for d in directions:
-                next_r, next_c = r+d[0], c+d[1]
-                if 0<=next_r<ROWS and 0<= next_c<COLS and grid[next_r][next_c] == 1:
+                next_r, next_c = r + d[0], c + d[1]
+                if (0<=next_r<ROWS) and (0 <= next_c < COLS) and grid[next_r][next_c] == 1:
                     dfs(next_r, next_c, grid)
-            
-        # if grid1 == 0 but grid2 == 1
-        # call dfs
-        # increment the counter
-        # count island in grid2
         
+        # if 0 in grid 1 and 1 in grid 2 -> can't be sub island
+        # call dfs
         for r in range(ROWS):
             for c in range(COLS):
                 if grid1[r][c] == 0 and grid2[r][c] == 1:
                     dfs(r,c,grid2)
-                
-        count = 0
+        
+        # everytime we see 1
+        # call dfs
+        # increment the counter
         for r in range(ROWS):
             for c in range(COLS):
-                if grid2[r][c] == 1:
-                    dfs(r,c, grid2)
-                    count += 1
-        return count
-        # return counter
+                if grid2[r][c] ==1:
+                    dfs(r,c,grid2)
+                    counter += 1
+        return counter
+        
