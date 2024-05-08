d9a2ee75a77b442eb95d6b86feb320abebd808cf
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-05-08 00:38:32 -0500

    Time: 692 ms (50.61%), Space: 53.6 MB (25.78%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,34 +1,29 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        # zip the input
-        # sort by start time O(n logn)
+        # sort & zip
         intervals = sorted(zip(startTime, endTime, profit))
-        # intervals.sort(key = lambda x:x[0])
+        # cache / memoization
         cache = {}
+        profit = 0
         
-        # cache
-        
-        # dfs(idx) + binary search 
-        # find the next start time using the end time of current job
-        # 1. include
-        # 2. not include
-        # keep track of max
-        def dfs(idx):
-            profit = 0
-            if idx == len(intervals):
+        # dfs
+        def dfs(i):
+        # keep track of max profit
+            if i == len(intervals):
                 return 0
-            if idx in cache:
-                return cache[idx]
-            
-            # don't take
-            # call dfs on next
-            profit = dfs(idx+1)
+            if i in cache:
+                return cache[i]
 
-            # take
-            # find the next start time
-            next_j = bisect.bisect(intervals, (intervals[idx][1], -1, -1))
-            cache[idx] = profit = max(profit, intervals[idx][2] + dfs(next_j))
+            # Don't take -> take the next
+            profit = dfs(i+1)
+            
+            # take -> find the next starting using binary search
+            next_idx = bisect.bisect(intervals, (intervals[i][1], -1, -1))
+            # call bfs on the next item
+            
+            cache[i] = profit = max(profit, intervals[i][2] + dfs(next_idx))
             return profit
         
         return dfs(0)
+                    
         
