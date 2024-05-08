2291e900490dc988f5d0dc71a382c0dd9f60f11e
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-05-08 16:12:35 -0500

    Time: 102 ms (29.25%), Space: 20.8 MB (21.49%) - LeetHub

--- 0296-best-meeting-point/0296-best-meeting-point.py
+++ 0296-best-meeting-point/0296-best-meeting-point.py
@@ -1,36 +1,29 @@
 class Solution:
     def minTotalDistance(self, grid: List[List[int]]) -> int:
-        # row, col variable
         ROWS, COLS = len(grid), len(grid[0])
-        # row, col, house list
-        row, col, houses = [],[], []
+        row, col, houses = [], [], []
         
-        # iterate through the grid
+        # add the point in order
+        # do one for row
         for r in range(ROWS):
             for c in range(COLS):
-        # find the house
                 if grid[r][c] == 1:
-                    houses.append((r,c))
                     row.append(r)
-        # add to the rwo and hourse list in a sorted order
-        
-        # do it col as well
+                    houses.append((r,c))
+                
+        # one for col
         for c in range(COLS):
             for r in range(ROWS):
-        # find the house
                 if grid[r][c] == 1:
                     col.append(c)
-                    
-        # find the median
-        # caculate dist from every hourse
-        # return the sum of it
-        median_r = row[len(row) // 2]
-        median_c = col[len(col) // 2]
         
-        res = 0
-        for h in houses:
-            res += abs(h[0]-median_r) + abs(h[1]-median_c)
-        return res
-        # clarifying question
-        # return val: dist from every house
-        # assume at least two houses always exist
+        # mid point
+        # mid from col and row
+        mid_r = row[len(row)//2]
+        mid_c = col[len(col)//2]
+        
+        dist = 0
+        for hr, hc in houses:
+            dist += abs(hr-mid_r) + abs(hc-mid_c)
+        return dist
+        
