81a4f840773ab9528e3372169f68d53e83f94cf9
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-05-09 14:55:49 -0500

    Time: 1870 ms (81.36%), Space: 24.4 MB (75.54%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.py
+++ 0827-making-a-large-island/0827-making-a-large-island.py
@@ -1,42 +1,71 @@
 class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
-        self.island_id = -1
-        self.island_areas = {}
-        
+        # variable: row, col, directions
+        # island dict
+        # island id
+        ROW, COL = len(grid), len(grid[0])
         self.directions = [(0,1), (0,-1), (1,0), (-1,0)]
-        ROWS, COLS = len(grid), len(grid[0])
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == 1:
-                    island_area = self.dfs(grid, r,c)
-                    self.island_areas[self.island_id] = island_area
-                    self.island_id -= 1
-        max_area = 0
+        self.island_area = {}
+        self.island_id = -1
         
-        for r in range(ROWS):
-            for c in range(COLS):
-                if not grid[r][c]:
+        # find island 
+        # update the island dict w island size
+        
+        for r in range(ROW):
+            for c in range(COL):
+                if grid[r][c] == 1:
+                    area = self.dfs(grid, r,c)
+                    self.island_area[self.island_id] = area
+                    self.island_id -=1
+        
+        # find empty land
+        # find surrounding island
+        # sum the surrounding
+        max_area =0
+        for r in range(ROW):
+            for c in range(COL):
+                if grid[r][c] == 0:
                     area = 1
                     surr = set()
                     for d in self.directions:
-                        next_r, next_c = r + d[0], c + d[1]
-                        if (0<= next_r <ROWS) and (0 <= next_c < COLS) and grid[next_r][next_c] != 0:
+                        next_r, next_c = r + d[0], c+ d[1]
+                        if (0<=next_r<ROW) and (0 <= next_c <COL) and grid[next_r][next_c] != 0:
                             surr.add(grid[next_r][next_c])
-                    for island_id in surr:
-                        area += self.island_areas[island_id]
-                    
-                    max_area = max(max_area, area)
+                    for s_id in surr:
+                        area += self.island_area[s_id]
+                max_area = max(max_area, area)
         return max_area if max_area else len(grid)**2
-        
-    def dfs(self, grid, r, c):
-        ROWS, COLS = len(grid), len(grid[0])
-        if (0 <= r < ROWS) and (0 <= c < COLS) and grid[r][c] == 1:
+        # dfs: class function
+        # grid, r, c
+        # find the island 
+        # update the grid w island id
+        # return the size of island
+    def dfs(self, grid, r,c):
+        ROW, COL = len(grid), len(grid[0])
+        if (0<=r<ROW) and (0<=c<COL) and grid[r][c] == 1:
             grid[r][c] = self.island_id
             area = 1
             for d in self.directions:
-                next_r, next_c = r + d[0], c + d[1]
-                area += self.dfs(grid, next_r, next_c)
+                next_r, next_c = r + d[0], c+d[1]
+                if (0<=next_r<ROW) and (0<=next_c<COL) and grid[next_r][next_c] == 1:
+                    area += self.dfs(grid, next_r, next_c)
+            
             return area
         else:
             return 0
-            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
