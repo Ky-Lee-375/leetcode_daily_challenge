76f2ef128e765a806cfb47326b596b4e319fe70d
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-05-09 10:26:40 -0500

    Time: 67 ms (39.46%), Space: 21 MB (47.09%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -6,52 +6,17 @@
 #         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        # edge case
-        if root is None:
-            return float('-inf')
-        
         res = float('-inf')
-        max_path = []
-
+        
         def dfs(node):
-            nonlocal res, max_path
-            if node is None:
-                return 0, []
+            nonlocal res
+            if node == None:
+                return 0
             
-            left_sum, left_path = dfs(node.left)
-            right_sum, right_path = dfs(node.right)
-
-            # Use max to discard negative paths
-            left_sum = max(0, left_sum)
-            right_sum = max(0, right_sum)
+            left = max(0, dfs(node.left))
+            right = max(0, dfs(node.right))
             
-            # Calculate current sum through this node
-            current_sum = node.val + left_sum + right_sum
-            if current_sum > res:
-                res = current_sum
-                # Reconstruct the path leading to this maximum
-                if left_sum > right_sum:
-                    max_path = left_path + [node.val] + list(reversed(right_path))
-                else:
-                    max_path = right_path + [node.val] + list(reversed(left_path))
-
-            # Return the max sum and path to parent
-            return_to_parent_sum = node.val + max(left_sum, right_sum)
-            if left_sum > right_sum:
-                return return_to_parent_sum, left_path + [node.val]
-            else:
-                return return_to_parent_sum, right_path + [node.val]
-        
+            res = max(res, node.val+left+right)
+            return node.val+max(left,right)
         dfs(root)
-        print("Maximum path sum is:", res)
-        print("Path for maximum sum:", max_path)
-        return res    
-        
-        # clarifying questions
-        # can root be empty
-        # can it have negative value
-        # binary tree
-        
-# print logest path
-
-        
+        return res
