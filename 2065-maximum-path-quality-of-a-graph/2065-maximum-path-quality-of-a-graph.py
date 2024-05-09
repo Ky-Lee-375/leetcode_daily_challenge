d5a2659220ce44ed830156aa5df518e47a31f162
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-05-09 00:38:39 -0500

    Time: 1262 ms (55.54%), Space: 18.2 MB (67.50%) - LeetHub

--- 2065-maximum-path-quality-of-a-graph/2065-maximum-path-quality-of-a-graph.py
+++ 2065-maximum-path-quality-of-a-graph/2065-maximum-path-quality-of-a-graph.py
@@ -1,25 +1,20 @@
 class Solution:
     def maximalPathQuality(self, values: List[int], edges: List[List[int]], maxTime: int) -> int:
-        # compose an undirected graph
-        G = collections.defaultdict(list)
+        # construct a graph
+        graph = collections.defaultdict(list)
         for u,v,w in edges:
-            G[u].append((v,w))
-            G[v].append((u,w))
+            graph[u].append((v,w))
+            graph[v].append((u,w))
+
         
-        # dfs(node, visited, gain, cost)
-        # recursively call dfs on neighbor
-        def dfs(node, visited, gain, cost):
+        # add curr val
+        def dfs(node, visited, gain, time):
             if node == 0:
                 self.ans = max(self.ans, gain)
-            for neighbor, w in G[node]:
-                if w <= cost:
-                    dfs(neighbor, visited | set([neighbor]), gain + (neighbor not in visited)*values[neighbor], cost-w)
+            for neighbor, w in graph[node]:
+                if w <= time:
+                    dfs(neighbor, visited|set([neighbor]), gain + (neighbor not in visited)*values[neighbor], time-w)
         
-        # self.ans = 0
-        # call dfs
-        # return self.ans
         self.ans = 0
         dfs(0, set([0]), values[0], maxTime)
         return self.ans
-        
-        
