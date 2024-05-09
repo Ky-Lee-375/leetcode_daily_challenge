3a3f785442366afde95a6cf26f83413563a828b0
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-05-09 01:59:00 -0500

    Time: 1693 ms (84.23%), Space: 16.8 MB (86.09%) - LeetHub

--- 0317-shortest-distance-from-all-buildings/0317-shortest-distance-from-all-buildings.py
+++ 0317-shortest-distance-from-all-buildings/0317-shortest-distance-from-all-buildings.py
@@ -1,38 +1,50 @@
 class Solution:
     def shortestDistance(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        dist_matrix = [[0]*cols for _ in range(rows)]
-        directions = [(1,0), (0,1), (-1,0), (0,-1)]
+        # variable declaration
+        ROWS, COLS = len(grid), len(grid[0])
+        directions = [(0,1), (0,-1), (1,0), (-1, 0)]
+        # maintain dist matrix
+        dist_matrix = [[0]*COLS for _ in range(ROWS)]
         
         BUILDING = 1
         OBSTACLE = 2
         EMPTY = 0
         
+        # final min dist
         min_dist = float('inf')
         
-        # BFS + queue
-        # if empty land is found, add to queue
-        # recursive BFS to neighbor node
-        
-        for row in range(rows):
-            for col in range(cols):
-                # if building is found
-                # call recusive dfs
+        # bfs
+        # everytime we traverse from one building, decrement
+        # update the dist matrix
+        # when we are done traversing, find the min from dist matrix
+        for row in range(ROWS):
+            for col in range(COLS):
+                # local distance 
+                local_dist = float('inf')
+                # if building, add to queue
                 if grid[row][col] == BUILDING:
-                    local_dist = float('inf')
-                    queue = collections.deque([(row, col, 0)])
-
-                    while queue:
-                        cur_row, cur_col, dist = queue.popleft()
+                    q = collections.deque([(row,col,0)])
+                
+                    # while queue
+                    # pop the item
+                    # move to 4 directions
+                    # update the distance
+                        # update dist_matrix (+=)
+                        # mark original grid -=1
+                    while q:
+                        cur_r, cur_c, dist = q.popleft()
                         for d in directions:
-                            next_row, next_col = cur_row + d[0], cur_col + d[1]
-                            if (0 <= next_row < rows) and (0 <= next_col<cols) and grid[next_row][next_col] == EMPTY:
-                                grid[next_row][next_col] -= 1
-                                dist_matrix[next_row][next_col] += dist + 1
-                                
-                                queue.append((next_row, next_col, dist+1))
-                                local_dist = min(local_dist, dist_matrix[next_row][next_col])
+                            next_r, next_c = cur_r + d[0], cur_c + d[1]
+                            if (0 <= next_r < ROWS) and (0 <= next_c < COLS) and grid[next_r][next_c] == EMPTY:
+                                grid[next_r][next_c] -= 1
+                                dist_matrix[next_r][next_c] += dist + 1
+
+                                # add the next to the queue
+                                # update the local dist
+                                q.append((next_r, next_c, dist+1))
+                                local_dist = min(local_dist, dist_matrix[next_r][next_c])
+                    # update empty
                     EMPTY -= 1
                     min_dist = local_dist
         return min_dist if min_dist != float('inf') else -1
-        
+                
