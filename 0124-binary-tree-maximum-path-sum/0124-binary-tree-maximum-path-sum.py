5720c496c2e22b73fe871cf14bdf1c6f45d5e802
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-05-09 10:32:57 -0500

    Time: 68 ms (34.08%), Space: 21.1 MB (19.63%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -6,17 +6,34 @@
 #         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        
         res = float('-inf')
+        max_path = []
         
         def dfs(node):
             nonlocal res
             if node == None:
-                return 0
+                return 0, []
             
-            left = max(0, dfs(node.left))
-            right = max(0, dfs(node.right))
+            left_sum, left_path = dfs(node.left)
+            right_sum, right_path = dfs(node.right)
             
-            res = max(res, node.val+left+right)
-            return node.val+max(left,right)
+            left_sum = max(0, left_sum)
+            right_sum = max(0, right_sum)
+            
+            curr_sum = node.val+left_sum+right_sum
+            if curr_sum > res:
+                res = curr_sum
+                if left_sum > right_sum:
+                    max_path = left_path + [node.val] + list(reversed(right_path))
+                else:
+                    max_path = right_path + [node.val] + list(reversed(left_path))
+                    
+            return_parent_sum = node.val + max(left_sum,right_sum)
+            
+            if left_sum >right_sum:
+                return return_parent_sum, left_path + [node.val]
+            else:
+                return return_parent_sum, right_path + [node.val]
         dfs(root)
         return res
