979d20b11cac33821be4e9f23ce302d777295c5a
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-05-09 20:08:20 -0500

    Time: 759 ms (60.55%), Space: 32 MB (42.45%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.py
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.py
@@ -1,21 +1,28 @@
 class Solution:
     def canSeePersonsCount(self, heights: List[int]) -> List[int]:
-        # monotonic stack
-        stack = []
-        res = [0] * len(heights)
+        # iterate from the back
+        # pop from stack only if next one is bigger
         
-        # loop backward
+        stack = []
+        result = [0] * len(heights)
+        
         for i in range(len(heights)-1, -1, -1):
             visible = 0
+            
+            # if stack exist, and curr is greater
+                # pop
+                # increment counter
             while stack and heights[i] > stack[-1]:
                 stack.pop()
-                visible += 1
+                visible +=1
             
+            # if exist
+                # increment counter
             if stack:
                 visible += 1
             
-            res[i] = visible
+            # add to result
+            # add to stack
             stack.append(heights[i])
-                
-        return res
-        
+            result[i] = visible
+        return result
