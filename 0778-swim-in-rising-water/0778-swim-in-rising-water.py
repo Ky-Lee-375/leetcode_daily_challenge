32a30e331256ac146e5efa1d861185e55d747e92
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-05-09 20:36:32 -0500

    Time: 75 ms (94.68%), Space: 17.2 MB (72.37%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -1,52 +1,25 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        # dijikstra + heap
-        # min heap (height, r, c)
+        # variable
+        ROWS, COLS = len(grid), len(grid[0])
+        directions = [(0,1), (0,-1),(1,0), (-1,0)]
+        # minheap (grid[r][c], r, c)
         # visit set
-        # direction
-        ROW, COL = len(grid), len(grid[0])
-        minH = [[grid[0][0], 0, 0]]
+        minHeap = [[grid[0][0], 0, 0]]
         visit = set()
-        directions = [(0,1), (0,-1), (1,0), (-1,0)]
         
-        # start from (0,0)
-        # add to visit set
         visit.add((0,0))
+        # loop through the grid
+        # if row, col is bottom right, return time
+        for r in range(ROWS):
+            for c in range(COLS):
+                t,r,c = heapq.heappop(minHeap)
+                if r == ROWS-1 and c == COLS-1:
+                    return t
+                
+                for d in directions:
+                    next_r, next_c = r + d[0], c + d[1]
+                    if (0 <= next_r <ROWS) and (0 <= next_c < COLS) and (next_r, next_c) not in visit:
+                        heapq.heappush(minHeap, [max(t, grid[next_r][next_c]), next_r, next_c])
+                        visit.add((next_r, next_c))
         
-        # while heap
-        # pop from heap
-        # if it's the right bottom grid, just return the height
-        while minH:
-            t, r, c = heapq.heappop(minH)
-            if r == ROW-1 and c == COL-1:
-                return t
-              
-            # if not, move 4 directions
-            # find the max
-                # add to visit set
-                # heap push
-            for d in directions:
-                next_r, next_c = r + d[0], c + d[1]
-                if (0<=next_r < ROW) and (0 <= next_c < COL) and (next_r, next_c) not in visit:
-                    visit.add((next_r,next_c))
-                    heapq.heappush(minH, [max(t, grid[next_r][next_c]), next_r, next_c])
-                
-                
-# space O(rowxcol)
-# time
-    # heap operation: log (mxn)
-    # traversal: worst case mxn
-    # O(mn log mn)
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
