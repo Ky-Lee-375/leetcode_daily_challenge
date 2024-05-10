f930cd5f51e70998abf1f72e1ae9ab8408de97cf
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-05-09 20:01:16 -0500

    Time: 47 ms (43.29%), Space: 18 MB (95.75%) - LeetHub

--- 1522-diameter-of-n-ary-tree/1522-diameter-of-n-ary-tree.py
+++ 1522-diameter-of-n-ary-tree/1522-diameter-of-n-ary-tree.py
@@ -12,24 +12,36 @@ class Solution:
         :type root: 'Node'
         :rtype: int
         """
+        # use two pointers
+        # save the longest, second longest
         best = 0
+        
+        # dfs on every child of the node
+        # compare w max1 and 2 
+        # update accordingly
         def dfs(node):
-            if node == None:
+            if node == 0:
                 return 0
-            max1 = 0
-            max2 = 0
+            nonlocal best
+            max1, max2 = 0,0
+            
             for child in node.children:
-                nonlocal best
                 depth = dfs(child)
+                # add +1 to include the current node
                 depth += 1
                 if depth > max1:
                     max1, max2 = depth, max1
                 elif depth > max2:
                     max2 = depth
-            best = max(best, max1+max2)
-            return max1
-        dfs(root)
-        return best
                 
+            best = max(best, max1+max2)
+        # keep track of best
+        # compare w taking max1 and max2
+        
+            return max1
+        # return the longest
         
         # call dfs on root
+        # return the best
+        dfs(root)
+        return best
