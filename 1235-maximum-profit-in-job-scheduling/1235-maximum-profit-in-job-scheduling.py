5ad3bcabd02812ad4e2dfa14bdd4468b0dd6f7af
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-05-10 01:17:38 -0500

    Time: 687 ms (52.86%), Space: 53.5 MB (26.68%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,25 +1,27 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        # zip the variable
-        intervals = sorted(zip(startTime, endTime, profit))
-        # cache
+        # variable declaration
+        # cache -> optimize
+        # sort & zip the variable
+        jobs = sorted(zip(startTime, endTime, profit))
         cache = {}
-        
-        # dfs + binary search
-            # don't take
-            # move to the next
-            # take -> find the next start
+        # dfs(job_id)
+        # if the job is the last -> return 0
+        # return the max
+
         def dfs(i):
-            if i == len(intervals):
+            if i == len(jobs):
                 return 0
             if i in cache:
                 return cache[i]
+            # not take -> next job
+            profit = dfs(i+1)
+
+            # take -> binary seach next searchtime
+            idx = bisect.bisect_left(jobs, (jobs[i][1], -1, -1))
+            cache[i] = profit = max(profit, jobs[i][2] + dfs(idx))
             
-            res = dfs(i+1)
-            
-            next_j = bisect.bisect_left(intervals, (intervals[i][1], -1,-1))
-            cache[i] = res = max(res, intervals[i][2] + dfs(next_j))
-            return res
+            return profit
         
-        # return max
         return dfs(0)
+        
