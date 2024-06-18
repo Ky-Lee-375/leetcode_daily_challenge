9229f09b761461b514ed8092c02aef82a2011114
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-06-18 11:49:51 -0500

    Time: 877 ms (7.29%), Space: 53.4 MB (27.96%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,27 +1,29 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        # variable declaration
-        # cache -> optimize
-        # sort & zip the variable
-        jobs = sorted(zip(startTime, endTime, profit))
+        # zip the variable so that I can keep track
+        # sort by startTime
+        intervals = sorted(zip(startTime, endTime, profit))
         cache = {}
-        # dfs(job_id)
-        # if the job is the last -> return 0
-        # return the max
-
+        
+        # recursive DFS
+        # two approach: 1) not take the job 2) take it and find the next one
         def dfs(i):
-            if i == len(jobs):
+            # base case: if it reaches the end
+            if i == len(intervals):
                 return 0
             if i in cache:
                 return cache[i]
-            # not take -> next job
-            profit = dfs(i+1)
-
-            # take -> binary seach next searchtime
-            idx = bisect.bisect_left(jobs, (jobs[i][1], -1, -1))
-            cache[i] = profit = max(profit, jobs[i][2] + dfs(idx))
             
-            return profit
+            # 1) not take the job
+            res = dfs(i+1)
+            
+            # 2) take the job
+            # binary search
+            idx = bisect.bisect_left(intervals, (intervals[i][1], -1,-1))
+            cache[i] = res = max(res, intervals[i][2]+ dfs(idx))
+            return res
         
         return dfs(0)
+        
+        
         
