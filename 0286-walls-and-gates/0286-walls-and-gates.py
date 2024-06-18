6acf18d11855d3926dded698c112db0b72eaf110
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-06-17 21:15:19 -0500

    Time: 227 ms (22.83%), Space: 22.9 MB (32.95%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -3,52 +3,34 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        # BFS + Queue
-        # invoke BFS simultaneously
-    
-        # variable
-        # ROW, COL
-        # direction
-        ROWS, COLS = len(rooms), len(rooms[0])
-        directions = [(0,1), (0,-1), (-1,0), (1,0)]
-        visit = set()
-        q = deque()
         
-        # go through the rooms
-        # add all buildings (== 0)
+        # invoke BFS on every gate
+        # increase the depth after one iteration
+        # use queue to initially store the gate
+        
+        # variable declaration
+        ROWS, COLS = len(rooms), len(rooms[0])
+        directions = [(0,1), (0,-1), (1,0), (-1,0)]
+        q = deque()
+        visit = set()
+        
         for r in range(ROWS):
             for c in range(COLS):
                 if rooms[r][c] == 0:
-                    visit.add((r,c))
                     q.append((r,c))
+                    visit.add((r,c))
         
-        # while queue
-        # len of queue
-            # add dist to grid
-        # increment the dist
         dist = 0
         while q:
             for i in range(len(q)):
                 r,c = q.popleft()
                 rooms[r][c] = dist
-                for d in directions:
-                    next_r, next_c = r + d[0], c + d[1]
-                    if (0 <= next_r <ROWS) and (0 <= next_c <COLS) and (next_r, next_c) not in visit and rooms[next_r][next_c] != -1:
-                        visit.add((next_r, next_c))
-                        q.append((next_r,next_c))
-            dist += 1
-                    
-                    
-                    
-                    
                 
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
+                for dr, dc in directions:
+                    next_r, next_c = r+dr, c + dc
+                    if (0 <= next_r < ROWS) and (0<=next_c<COLS) and rooms[next_r][next_c] != -1 and (next_r, next_c) not in visit:
+                        visit.add((next_r,next_c))
+                        q.append((next_r,next_c))
+            dist+=1
+        
+        
