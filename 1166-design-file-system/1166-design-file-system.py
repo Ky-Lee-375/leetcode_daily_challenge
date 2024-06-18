dbfec119da1ad4f0ebea6207529aba1a82152114
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-06-17 22:55:05 -0500

    Time: 245 ms (17.97%), Space: 24.2 MB (68.58%) - LeetHub

--- 1166-design-file-system/1166-design-file-system.py
+++ 1166-design-file-system/1166-design-file-system.py
@@ -1,37 +1,29 @@
 class FileSystem:
 
     def __init__(self):
-        # path dict
+        #path
         self.paths = collections.defaultdict()
         
 
     def createPath(self, path: str, value: int) -> bool:
-        # if already exists -> false
         if path in self.paths:
             return False
-
-        # see if parent path exists
-        # if not -> false
-        idx = path.rfind('/')
-        if path[:idx] not in self.paths and len(path[:idx]) > 1:
-            return False
         
-        # create one
-        # return true
+        # rfind: returns highest idx value of last occurrence
+        idx = path.rfind('/')
+        
+        # if parent path doesnt exist -> return false
+        if path[:idx] not in self.paths and len(path[:idx]) >1:
+            return False
         self.paths[path] = value
-        return True        
+        return True
+        
 
     def get(self, path: str) -> int:
-        # get path if in path dict
         return self.paths.get(path) if path in self.paths else -1
-        
 
 
 # Your FileSystem object will be instantiated and called as such:
 # obj = FileSystem()
 # param_1 = obj.createPath(path,value)
 # param_2 = obj.get(path)
-
-
-# clarifying Q
-# all lowercase?
