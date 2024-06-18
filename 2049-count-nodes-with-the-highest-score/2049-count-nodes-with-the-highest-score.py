b3b551d837e506872238c75197b50281b81633b9
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-06-18 12:57:24 -0500

    Time: 926 ms (47.12%), Space: 62.6 MB (43.41%) - LeetHub

--- 2049-count-nodes-with-the-highest-score/2049-count-nodes-with-the-highest-score.py
+++ 2049-count-nodes-with-the-highest-score/2049-count-nodes-with-the-highest-score.py
@@ -1,15 +1,22 @@
 class Solution:
     def countHighestScoreNodes(self, parents: List[int]) -> int:
-        # construct a graph
+        # compute the score
+        # return # of nodes w highest score
+        
+        # create a graph from a list
+        # using enumerate
         graph = collections.defaultdict(list)
-        for node, parent in enumerate(parents):
-            graph[parent].append(node)
+        for idx, parent in enumerate(parents):
+            graph[parent].append(idx)
+
         d = collections.Counter()
         n = len(parents)
-        # dfs
-        # find the score from the leaf
+        
+        # Using dfs
+        # size of two subtree
+        # multiply size of two subtrees
         def dfs(node):
-            total, product = 0, 1
+            total, product = 0,1
             for child in graph[node]:
                 res = dfs(child)
                 total += res
@@ -18,6 +25,9 @@ class Solution:
             product *= max(1, n-total-1)
             d[product] += 1
             return total +1
-        
         dfs(0)
         return d[max(d.keys())]
+            
+        
+        
+        
