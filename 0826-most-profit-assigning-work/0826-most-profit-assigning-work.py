6f254c2158dfded5f046f56a0d23d1ac0032f38e
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-06-18 12:31:41 -0500

    Time: 333 ms (32.75%), Space: 19.3 MB (93.65%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
@@ -1,20 +1,15 @@
 class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
-        # zip difficulty and profit
-        # sort by difficulty
+        # sort and zip by difficulty
         jobs = sorted(zip(difficulty, profit))
         worker = sorted(worker)
         
-        # sort worker array
-        # while loop
-        # find the max diff per worker
-        cur = j = res = 0
+        # find the highest difficulty a worker can do
+        cur = j = profit = 0
         for i in range(len(worker)):
-            w = worker[i]
-            while j < len(jobs) and w >= jobs[j][0]:
+            while j < len(jobs) and worker[i] >= jobs[j][0]:
                 cur = max(cur, jobs[j][1])
-                j+=1
-            res += cur
-        # add the profit
-        # return the profit
-        return res
+                j += 1
+            profit += cur
+        return profit
+        
