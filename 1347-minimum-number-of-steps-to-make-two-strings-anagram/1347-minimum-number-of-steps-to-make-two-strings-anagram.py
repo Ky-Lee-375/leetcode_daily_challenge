869fb22e7c378b8f9a33c521695a31e57d10ede6
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-06-18 15:54:32 -0500

    Time: 212 ms (13.84%), Space: 16.9 MB (96.49%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,18 +1,13 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        # create a dict w all letter
         d = dict.fromkeys(string.ascii_lowercase, 0)
         
-        # increment for s
-        # decrement for t
         for i in range(len(s)):
             d[s[i]] += 1
             d[t[i]] -= 1
         
-        # go through the dict
-        # sum the positive
         res = 0
-        for k in d.keys():
-            res += max(0, d[k])
+        for v in d.values():
+            if v >= 0:
+                res += v
         return res
-        
