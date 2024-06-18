dafb4dfb6b22996b39545dad08bc03221da38666
Ky-Lee-375 <80299116+Ky-Lee-375@users.noreply.github.com>
2024-06-18 13:55:19 -0500

    Time: 2407 ms (17.48%), Space: 33.5 MB (60.90%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -1,33 +1,32 @@
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        # variable
         ROWS, COLS = len(grid1), len(grid1[0])
-        counter = 0
         directions = [(0,1), (0,-1), (1,0), (-1,0)]
         
-        # dfs(r,c,grid)
-        # from r,c change all 1 to 0
-        def dfs(r,c, grid):
+        # dfs that mark non subisland as 0
+        def dfs(r, c, grid):
             grid[r][c] = 0
             
             for d in directions:
                 next_r, next_c = r + d[0], c + d[1]
-                if (0<=next_r<ROWS) and (0 <= next_c < COLS) and grid[next_r][next_c] == 1:
+                if (0<=next_r<ROWS) and (0<=next_c<COLS) and grid[next_r][next_c] == 1:
                     dfs(next_r, next_c, grid)
+            
         
-        # if 0 in grid 1 and 1 in grid 2 -> can't be sub island
-        # call dfs
+        # loop through grid2
+        # call dfs if grid1 == 0 but grid2 == 1
         for r in range(ROWS):
             for c in range(COLS):
                 if grid1[r][c] == 0 and grid2[r][c] == 1:
                     dfs(r,c,grid2)
         
-        # everytime we see 1
-        # call dfs
-        # increment the counter
+        # loop through grid2
+        # count num island remaining
+        counter = 0
         for r in range(ROWS):
             for c in range(COLS):
-                if grid2[r][c] ==1:
+                if grid2[r][c] == 1:
                     dfs(r,c,grid2)
-                    counter += 1
+                    counter +=1
         return counter
-        
